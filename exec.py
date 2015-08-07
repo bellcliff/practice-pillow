5a93a5ac154a16bf33899c84eefe09e7f1a86c83
Yang Bo <bell.cliff@gmail.com>
2015-08-08 00:07:39 +0800

    clean code

--- exec.py
+++ exec.py
@@ -3,7 +3,7 @@
 # @Author: yangbo
 # @Date:   2015-08-04 11:48:39
 # @Last Modified by:   Yang Bo
-# @Last Modified time: 2015-08-07 22:57:30
+# @Last Modified time: 2015-08-08 00:02:38
 
 import os
 import numpy
@@ -12,68 +12,70 @@ from PIL import Image
 from uiautomator import Device
 
 
-DEVICE = Device('192.168.57.101:5555')
+DEVICE = Device('192.168.57.102:5555')
 RECTS = {
-    'back': (90, 430, 230, 470),
-    'item': (170, 560, 315, 600),
-    'continue': (60, 700, 210, 740),
-    'start': (50, 730, 180, 770),
-    'game': (130, 730, 230, 760),
-    'go': (180, 740, 300, 760)
+    'back': (190, 700, 280, 720),
+    'item': (295, 890, 425, 925),
+    'continue': (125, 1125, 270, 1165),
+    'start': (55, 1135, 150, 1175),
+    #    'burn': (610, 675, 650, 695),
+    'fight': (315, 1115, 400, 1155)
 }
+PATH_RES = 'resource'
 
 
-def click(name):
-    assert name in RECTS
-    box = RECTS[name]
-    x = int((box[0] + box[2])/2)
-    y = int((box[1] + box[3])/2)
-    DEVICE.click(x, y)
+class Crop:
+    PATH_RES = 'resource'
+    CACHE = dict()
 
+    def __init__(self, name):
+        self.name = name
+        self. cropPath = '%s/%s_crop.png' % (Crop.PATH_RES, self.name)
+        self.cropImg = Image.open(self.cropPath)
+        self.box = RECTS[name]
 
-def compareFile(a, b, box):
-    a = a.crop(box)
-    for x in range(0, a.size[0]):
-        for y in range(0, a.size[1]):
-            p = (x, y)
-            pa = a.getpixel(p)
-            pb = b.getpixel(p)
-            if pa != pb:
-                return False
-    return True
+    @property
+    def rawPath(self):
+        return '%s/%s.png' % (self.PATH_RES, self.name)
 
+    @property
+    def cropPath(self):
+        return '%s/%s_crop.png' % (self.PATH_RES, self.name)
 
-def compare(img, cropName):
-    cropImg = Image.open('screen/%s_crop.png' % cropName)
-    return compareFile(img, cropImg, RECTS[cropName])
+    def compare(self, img):
+        a = img.crop(self.box)
+        for x in range(0, a.size[0]):
+            for y in range(0, a.size[1]):
+                p = (x, y)
+                pa = a.getpixel(p)
+                pb = self.cropImg.getpixel(p)
+                if pa != pb:
+                    return False
+        return True
 
+    def click(self):
+        x = int((self.box[0] + self.box[2])/2)
+        y = int((self.box[1] + self.box[3])/2)
+        DEVICE.click(x, y)
 
-def crop(imgName):
-    assert imgName in RECTS
-    box = RECTS[imgName]
-    imgPath = 'screen/%s.png' % imgName
-    imgSrc = Image.open(imgPath).crop(box)
-    imgCropPath = 'screen/%s_crop.png' % imgName
-    Image.fromarray(numpy.array(imgSrc)).save(imgCropPath)
+    @staticmethod
+    def instance(name):
+        if name not in Crop.CACHE:
+            Crop.CACHE[name] = Crop(name)
+        return Crop.CACHE[name]
 
-
-def action():
-    img, imgPath = screen()
-    isItem = False
-    for n, box in RECTS.items():
-        if compare(img, n):
-            print 'found %s' % n
-            isItem = n in ['item', 'continue']
-            click(n)
-            break
-    if not isItem:
-        os.remove(imgPath)
-
-
-def printTime():
-    import time
-    millis = int(round(time.time() * 1000))
-    print millis
+    @staticmethod
+    def act():
+        img, path = screen()
+        keepScreen = False
+        for name in RECTS.keys():
+            crop = Crop.instance(name)
+            if crop.compare(img):
+                crop.click()
+                keepScreen = name in ['item']
+                break
+        if not keepScreen:
+            os.remove(path)
 
 
 def screen(screenName=None, path='imgs'):
@@ -83,12 +85,8 @@ def screen(screenName=None, path='imgs'):
     DEVICE.screenshot(screenPath)
     return Image.open(screenPath), screenPath
 
-
-def run():
-    while True:
-        action()
-        time.sleep(3)
-
-
 if __name__ == '__main__':
-    run()
+    while True:
+        Crop.act()
+        time.sleep(1)
+    # screen('continue.png', 'resource')
